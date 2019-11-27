acd2f4cb0b4b9853237c024d97636ee7e80252f6
chenzipeng10 <chenzipeng10@126.com>
2019-11-27 12:20:02 +0800

    "LeetCode 198 & LeetCode 213"

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.locks.LockSupport;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("begin park!");
+        LockSupport.unpark(Thread.currentThread());
+        LockSupport.park();
+        System.out.println("end park!");
+    }
+}
