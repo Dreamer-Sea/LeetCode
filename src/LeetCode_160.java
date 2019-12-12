fbb877408adec3595cd15d52b4091fecb96bc54c
chenzipeng10 <chenzipeng10@126.com>
2019-12-12 16:24:23 +0800

    160 & 599 & 23

---
+++ src/LeetCode_160.java
@@ -0,0 +1,11 @@
+public class LeetCode_160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        if (headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        while (a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
