07f4103b210456ccb92168483a29b6976710b056
chenzipeng10 <chenzipeng10@126.com>
2019-12-20 15:16:50 +0800

    221, 53

--- src/LeetCode_53.java
+++ src/LeetCode_53.java
@@ -1,10 +1,4 @@
 public class LeetCode_53 {
-
-    public static void main(String[] args) {
-        LeetCode_53 p = new LeetCode_53();
-        p.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
-    }
-
     public int maxSubArray(int[] nums){
         if (nums.length == 0) return 0;
         int max = nums[0], sum = max;
