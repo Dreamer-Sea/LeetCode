ea43f986300055843cbca2bfe55079a7f562f009
chenzipeng10 <chenzipeng10@126.com>
2019-12-24 13:34:39 +0800

    3, 151

--- src/LeetCode_151.java
+++ src/LeetCode_151.java
@@ -1,27 +1,14 @@
 public class LeetCode_151 {
-
-    public static void main(String[] args) {
-        LeetCode_151 p = new LeetCode_151();
-        System.out.println(p.reverseWords("a good   example"));
-    }
-
     public String reverseWords(String s){
-        if (s == null || s.length() == 0) return "";
-        String[] strs = s.trim().split(" ");
-        StringBuilder sb = new StringBuilder();
-        for (int i = strs.length - 1; i >= 0; i--){
-            String str = strs[i];
-            while (str.contains(" ")) str = str.replaceAll(" ", "");
+        if (s.length() == 0) return s;
+        s = s.trim();
+        String[] strs = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = strs.length-1; i >= 0; i--){
             if ("".equals(strs[i])) continue;
-            sb.append(strs[i]);
-            if (i != 0) sb.append(" ");
+            builder.append(strs[i]);
+            if (i != 0) builder.append(" ");
         }
-        return sb.toString();
-    }
-
-    private void swap(String[] strs, int x, int y){
-        String tmp = strs[x];
-        strs[x] = strs[y];
-        strs[y] = tmp;
+        return builder.toString();
     }
 }
--- src/LeetCode_3.java
+++ src/LeetCode_3.java
@@ -1,11 +1,11 @@
 public class LeetCode_3 {
     public int lengthOfLongestSubstring(String s){
-        int ans = 0, n = s.length();
-        if (n == 0) return 0;
+        if (s.length() == 0) return 0;
         int[] index = new int[128];
-        for (int i = 0, j = 0; i < n; i++){
+        int ans = 0;
+        for (int i = 0, j = 0; i < s.length(); i++){
             j = Math.max(index[s.charAt(i)], j);
-            ans = Math.max(ans, i-j+1);
+            ans = Math.max(ans, i - j + 1);
             index[s.charAt(i)] = i+1;
         }
         return ans;
