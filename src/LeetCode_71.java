76c03a1f3f979b8790e16d601ece35045e97eb55
chenzipeng10 <chenzipeng10@126.com>
2019-12-24 15:01:59 +0800

    71

--- src/LeetCode_71.java
+++ src/LeetCode_71.java
@@ -1,14 +1,15 @@
-import java.util.LinkedList;
 import java.util.Stack;
 
 public class LeetCode_71 {
 
     public static void main(String[] args) {
         LeetCode_71 p = new LeetCode_71();
-        System.out.println(p.simplifyPath("/a/../../b/../c//.//"));;
+        String path = "/home/";
+        System.out.println(p.simplifyPath(path));
     }
 
     public String simplifyPath(String path){
+        if (path == null) return null;
         String[] strs = path.split("/");
         Stack<String> stack = new Stack<>();
         for (int i = 0; i < strs.length; i++){
@@ -16,12 +17,13 @@ public class LeetCode_71 {
                 stack.pop();
             else if (!"".equals(strs[i]) && !".".equals(strs[i]) && !"..".equals(strs[i]))
                 stack.push(strs[i]);
+
         }
         if (stack.isEmpty()) return "/";
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < stack.size(); i++){
-            sb.append("/" + stack.get(i));
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++){
+            builder.append("/" + stack.get(i));
         }
-        return sb.toString();
+        return builder.toString();
     }
 }
