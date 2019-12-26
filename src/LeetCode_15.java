a5f947b2d72f38f7fb25f4a2178a6580a3b5349a
chenzipeng10 <chenzipeng10@126.com>
2019-12-26 15:26:28 +0800

    15, 695

--- src/LeetCode_15.java
+++ src/LeetCode_15.java
@@ -3,15 +3,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode_15 {
-
-    public static void main(String[] args) {
-        LeetCode_15 p = new LeetCode_15();
-        List<List<Integer>> res = new ArrayList<>();
-        res = p.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
-        for (List list : res)
-            System.out.println(list);
-    }
-
     public List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> res = new ArrayList<>();
         int len = nums.length;
--- src/LeetCode_695.java
+++ src/LeetCode_695.java
@@ -1,10 +1,5 @@
 public class LeetCode_695 {
 
-    public static void main(String[] args) {
-        LeetCode_695 p = new LeetCode_695();
-        p.maxAreaOfIsland(new int[][]{{0, 1}});
-    }
-
     public int maxAreaOfIsland(int[][] grid){
         if (grid.length == 0 || grid[0].length == 0) return 0;
         int max = 0;
@@ -22,13 +17,12 @@ public class LeetCode_695 {
     }
 
     private void dfs(int[][] grid, int x, int y, int[] count){
-        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != 1)
-            return;
-        grid[x][y] = -1;
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) return;
+        grid[x][y] = 0;
         count[0]++;
         dfs(grid, x+1, y, count);
-        dfs(grid, x, y+1, count);
         dfs(grid, x-1, y, count);
+        dfs(grid, x, y+1, count);
         dfs(grid, x, y-1, count);
     }
 }
