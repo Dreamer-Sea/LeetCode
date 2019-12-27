bb9e07dd854adff8e191e6df6a3cf9641380abdf
chenzipeng10 <chenzipeng10@126.com>
2019-12-27 17:01:14 +0800

    60

--- src/LeetCode_60.java
+++ src/LeetCode_60.java
@@ -1,27 +1,19 @@
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class LeetCode_60 {
-
-    public static void main(String[] args) {
-        LeetCode_60 p = new LeetCode_60();
-        System.out.println(p.getPermutation(3, 2));
-    }
-
     public String getPermutation(int n, int k){
-        List<Integer> num = new ArrayList<>();
+        ArrayList<Integer> nums = new ArrayList<>();
         StringBuilder builder = new StringBuilder();
         int fact = 1;
         for (int i = 1; i <= n; i++){
             fact *= i;
-            num.add(i);
+            nums.add(i);
         }
         k--;
         for (int i = 0; i < n; i++){
             fact /= (n - i);
-            int idx = (k / fact);
-            builder.append(num.remove(idx));
+            int idx = k / fact;
+            builder.append(nums.remove(idx));
             k -= idx * fact;
         }
         return builder.toString();
