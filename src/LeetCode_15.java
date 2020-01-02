112ca9b2d282784a13d15f85329d5173c9ce1576
chenzipeng10 <chenzipeng10@126.com>
2020-01-02 15:37:52 +0800

    15, 695, 33

--- src/LeetCode_15.java
+++ src/LeetCode_15.java
@@ -5,23 +5,25 @@ import java.util.List;
 public class LeetCode_15 {
     public List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> res = new ArrayList<>();
-        int len = nums.length;
-        if (nums == null || len < 3) return res;
+        if (nums == null || nums.length == 0) return res;
         Arrays.sort(nums);
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < nums.length; i++){
             if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            int L = i + 1, R = len - 1;
-            while (L < R){
-                int sum = nums[i] + nums[L] + nums[R];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0){
-                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
-                    while (L < R && nums[L] == nums[L+1]) L++;
-                    while (L < R && nums[R] == nums[R-1]) R--;
-                    L++;
-                    R--;
-                }else if (sum > 0) R--;
-                else L++;
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }else if (sum > 0){
+                    right--;
+                }else{
+                    left++;
+                }
             }
         }
         return res;
--- src/LeetCode_695.java
+++ src/LeetCode_695.java
@@ -1,8 +1,8 @@
 public class LeetCode_695 {
     public int maxAreaOfIsland(int[][] grid){
         if (grid == null || grid.length == 0) return 0;
-        int[] count = new int[1];
         int max = 0;
+        int[] count = new int[1];
         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid[0].length; j++){
                 if (grid[i][j] == 1){
