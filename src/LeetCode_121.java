b48e7a3fe06f0e4110b93fc56983d7215cf0b936
chenzipeng10 <chenzipeng10@126.com>
2020-01-03 15:32:10 +0800

    121, 122, 674

--- src/LeetCode_121.java
+++ src/LeetCode_121.java
@@ -1,7 +1,7 @@
 public class LeetCode_121 {
     public int maxProfit(int[] prices){
-        if (prices.length == 0) return 0;
-        int profit = 0, min = prices[0];
+        if (prices == null || prices.length == 0) return 0;
+        int min = prices[0], profit = 0;
         for (int i = 1; i < prices.length; i++){
             if (min > prices[i]) min = prices[i];
             profit = Math.max(profit, prices[i] - min);
--- src/LeetCode_674.java
+++ src/LeetCode_674.java
@@ -2,12 +2,12 @@ public class LeetCode_674 {
 
     public static void main(String[] args) {
         LeetCode_674 p = new LeetCode_674();
-        p.findLengthOfLCIS(new int[]{1, 3, 5, 4, 7});
+        p.findLengthOfLCIS(new int[]{1, 3, 5, 7, 9});
     }
 
     public int findLengthOfLCIS(int[] nums){
         if (nums == null || nums.length == 0) return 0;
-        int count = 1, max = 1;
+        int max = 1, count = 1;
         for (int i = 1; i < nums.length; i++){
             if (nums[i] > nums[i-1]) count++;
             else count = 1;
