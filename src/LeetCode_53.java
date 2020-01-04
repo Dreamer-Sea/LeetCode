08d846c6068cc82fa24c350f3b0226882355c182
chenzipeng10 <chenzipeng10@126.com>
2020-01-04 20:29:34 +0800

    53

--- src/LeetCode_53.java
+++ src/LeetCode_53.java
@@ -1,11 +1,17 @@
 public class LeetCode_53 {
+
+    public static void main(String[] args) {
+        LeetCode_53 p = new LeetCode_53();
+        p.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    }
+
     public int maxSubArray(int[] nums){
         if (nums.length == 0) return 0;
-        int max = nums[0], sum = max;
+        int max = nums[0], tmp = nums[0];
         for (int i = 1; i < nums.length; i++){
-            if (nums[i] > nums[i] + sum) sum = 0;
-            sum += nums[i];
-            max = Math.max(max, sum);
+            if (tmp + nums[i] < nums[i]) tmp = 0;
+            tmp += nums[i];
+            if (max < tmp) max = tmp;
         }
         return max;
     }
