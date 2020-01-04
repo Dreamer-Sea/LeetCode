81bdd4b04c1eb6e269acd821df06f718ae3cc4ed
chenzipeng10 <chenzipeng10@126.com>
2020-01-04 20:06:17 +0800

    221

--- src/LeetCode_221.java
+++ src/LeetCode_221.java
@@ -1,10 +1,11 @@
 public class LeetCode_221 {
     public int maximalSquare(char[][] matrix){
-        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[] dp = new int[cols+1];
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length;
         int maxLen = 0, prev = 0;
-        for (int i = 1; i <= rows; i++){
-            for (int j = 1; j <= cols; j++){
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
                 int tmp = dp[j];
                 if (matrix[i-1][j-1] == '1'){
                     dp[j] = Math.min(Math.min(dp[j-1], prev), dp[j]) + 1;
