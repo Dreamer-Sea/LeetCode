06510d513d8871e68aa086dd30e73f3db9e0ec37
chenzipeng10 <chenzipeng10@126.com>
2020-01-05 11:45:33 +0800

    120, 155, 146

--- src/LeetCode_120.java
+++ src/LeetCode_120.java
@@ -1,12 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode_120 {
+
+    public static void main(String[] args) {
+        LeetCode_120 p = new LeetCode_120();
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(2));
+        triangle.add(Arrays.asList(3, 4));
+        triangle.add(Arrays.asList(6, 5, 7));
+        triangle.add(Arrays.asList(4, 1, 8, 3));
+        p.minimumTotal(triangle);
+    }
+
     public int minimumTotal(List<List<Integer>> triangle){
-        int row = triangle.size();
-        int[] dp = new int[row+1];
-        for (int level = row-1; level >= 0; level--){
-            for (int i = 0; i <= level; i++){
-                dp[i] = Math.min(dp[i], dp[i+1]) + triangle.get(level).get(i);
+        if (triangle.size() == 0) return 0;
+        int m = triangle.size(), n = triangle.get(m-1).size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++){
+            dp[i] = triangle.get(m-1).get(i);
+        }
+        for (int i = m-2; i >= 0; i--){
+            for (int j = 0; j < i+1; j++){
+                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
             }
         }
         return dp[0];
--- src/LeetCode_155.java
+++ src/LeetCode_155.java
@@ -1,18 +1,19 @@
 import java.util.Stack;
 
 public class LeetCode_155 {
-    class MinStack{
+    class MinStack {
 
         private Stack<Integer> stack;
         private int min;
 
+        /** initialize your data structure here. */
         public MinStack() {
             stack = new Stack<>();
             min = Integer.MAX_VALUE;
         }
 
         public void push(int x) {
-            if (min >= x){
+            if (x <= min){
                 stack.push(min);
                 min = x;
             }
@@ -32,21 +33,5 @@ public class LeetCode_155 {
         public int getMin() {
             return min;
         }
-    }
-
-    public static void main(String[] args) {
-        LeetCode_155 p = new LeetCode_155();
-        p.test();
-    }
-
-    private MinStack stack = new MinStack();
-
-    private void test(){
-        stack.push(0);
-        stack.push(1);
-        stack.push(0);
-        stack.getMin();
-        stack.pop();
-        stack.getMin();
     }
 }
