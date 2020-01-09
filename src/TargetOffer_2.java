c524345f28c8508b154dee802124ab438972be29
chenzipeng10 <chenzipeng10@126.com>
2020-01-09 23:23:01 +0800

    TargetOffer_2

---
+++ src/TargetOffer_2.java
@@ -0,0 +1,22 @@
+public class TargetOffer_2 {
+    public String replaceSpace(StringBuffer str){
+        int P1 = str.length() - 1;
+        for (int i = 0; i <= P1; i++){
+            if (str.charAt(i) == ' '){
+                str.append("  ");
+            }
+        }
+        int P2 = str.length() - 1;
+        while (P1 >= 0 && P1 < P2){
+            char c = str.charAt(P1--);
+            if (c == ' '){
+                str.setCharAt(P2--, '0');
+                str.setCharAt(P2--, '2');
+                str.setCharAt(P2--, '%');
+            }else{
+                str.setCharAt(P2--, c);
+            }
+        }
+        return str.toString();
+    }
+}
