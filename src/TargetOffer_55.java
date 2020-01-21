e811cf4efd52adc6fb3b83a3d3feaf574b83c613
chenzipeng10 <chenzipeng10@126.com>
2020-01-21 14:13:58 +0800

    TargetOffer_55

---
+++ src/TargetOffer_55.java
@@ -0,0 +1,22 @@
+public class TargetOffer_55 {
+    public ListNode EntryNodeOfLoop(ListNode head) {
+        if (head == null) return null;
+        boolean flag = false;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) return null;
+        fast = head;
+        while (fast != slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
