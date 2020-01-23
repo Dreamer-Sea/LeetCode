c459faa408002a2f1d58c6a9606dcd04994493ed
chenzipeng10 <chenzipeng10@126.com>
2020-01-23 22:27:01 +0800

    TargetOffer-18

---
+++ src/TargetOffer_18.java
@@ -0,0 +1,10 @@
+public class TargetOffer_18 {
+    public void Mirror(TreeNode root) {
+        if (root == null) return;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
