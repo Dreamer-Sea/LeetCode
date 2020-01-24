0eecf9932b5927e18e206ef89b984e20ddad2192
chenzipeng10 <chenzipeng10@126.com>
2020-01-24 23:13:26 +0800

    TargetOffer_20

---
+++ src/TargetOffer_20.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class TargetOffer_20 {
+
+    private Stack<Integer> stack = new Stack<>();
+    private int min = Integer.MAX_VALUE;
+
+    public void push(int node) {
+        if (node < min){
+            stack.push(min);
+            min = node;
+        }
+        stack.push(node);
+    }
+
+    public void pop() {
+        if (stack.pop() == min){
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return min;
+    }
+}
