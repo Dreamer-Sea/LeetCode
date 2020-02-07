f4796a90784123c6a0240a0213e2d2dc0c009118
chenzipeng10 <chenzipeng10@126.com>
2020-02-07 17:49:05 +0800

    TargetOffer_40

---
+++ src/TargetOffer_40.java
@@ -0,0 +1,12 @@
+public class TargetOffer_40 {
+    public void FindNumsAppearOnce(int[] nums,int[] num1 , int[] num2) {
+        if (nums == null || nums.length == 0) return;
+        int xor = 0, flag = 1;
+        for (int i = 0; i < nums.length; i++) xor ^= nums[i];
+        while ((xor & flag) == 0) flag <<= 1;
+        for (int i = 0; i < nums.length; i++){
+            if ((flag & nums[i]) == 0) num1[0] ^= nums[i];
+            else num2[0] ^= nums[i];
+        }
+    }
+}
