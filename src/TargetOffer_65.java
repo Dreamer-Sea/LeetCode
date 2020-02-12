d99d553dd905d6125d1f25e297ec844ab55ef76a
chenzipeng10 <chenzipeng10@126.com>
2020-02-12 09:34:59 +0800

    TargetOffer_65

--- src/TargetOffer_65.java
+++ src/TargetOffer_65.java
@@ -1,35 +1,31 @@
 public class TargetOffer_65 {
-    public static void main(String[] args) {
-        hasPath("ABCESFCSADEE".toCharArray(), 3, 4, "SEE".toCharArray());
-    }
-
-    public static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        if (matrix == null || matrix.length == 0 || rows * cols != matrix.length || matrix.length < str.length) return false;
-        boolean[] marked = new boolean[rows * cols];
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
+        if (matrix == null || matrix.length == 0 || matrix.length != rows * cols || str == null || str.length == 0 || str.length > matrix.length)
+            return false;
+        boolean[] visited = new boolean[rows * cols];
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < cols; j++){
-                if (backtracking(matrix, rows, cols, str, i, j, 0, marked))
-                    return true;
+                if (backtracking(matrix, rows, cols, str, i, j, 0, visited)) return true;
             }
         }
         return false;
     }
 
-    private static boolean backtracking(char[] matrix, int rows, int cols, char[] str, int row, int col, int idx, boolean[] marked){
+    private boolean backtracking(char[] matrix, int rows, int cols, char[] str, int row, int col, int idx, boolean[] visited){
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return false;
         boolean flag = false;
-        if (col < 0 || col >= cols || row < 0 || row >= rows) return false;
-        int i = row * cols + col;
-        if (col >= 0 && col < cols && row >= 0 && row < rows && !marked[i] && matrix[i] == str[idx]){
-            marked[i] = true;
+        int i = cols * row + col;
+        if (!visited[i] && matrix[i] == str[idx]){
+            visited[i] = true;
             idx++;
             if (idx == str.length) return true;
-            flag = backtracking(matrix, rows, cols, str, row+1, col, idx, marked)
-                    || backtracking(matrix, rows, cols, str, row-1, col, idx, marked)
-                    || backtracking(matrix, rows, cols, str, row, col+1, idx, marked)
-                    || backtracking(matrix, rows, cols, str, row, col-1, idx, marked);
+            flag = backtracking(matrix, rows, cols, str, row+1, col, idx, visited)
+                    || backtracking(matrix, rows, cols, str, row-1, col, idx, visited)
+                    || backtracking(matrix, rows, cols, str, row, col+1, idx, visited)
+                    || backtracking(matrix, rows, cols, str, row, col-1, idx, visited);
             if (!flag){
                 idx--;
-                marked[i] = false;
+                visited[i] = false;
             }
         }
         return flag;
