d4ff32384f3fcd6916a7743494fe9170f762fb6a
chenzipeng10 <chenzipeng10@126.com>
2020-02-12 10:33:58 +0800

    TargetOffer_66

--- src/TargetOffer_66.java
+++ src/TargetOffer_66.java
@@ -1,38 +1,32 @@
 public class TargetOffer_66 {
-
-    public static void main(String[] args) {
-        movingCount(18, 10, 10);
-    }
-
-    public static int movingCount(int threshold, int rows, int cols){
+    public int movingCount(int threshold, int rows, int cols) {
         if (threshold < 0 || rows < 1 || cols < 1) return 0;
-        int[] count = new int[1];
-        boolean[] marked = new boolean[rows * cols];
-        return backtracking(threshold, rows, cols, marked, 0, 0);
+        boolean[] visited = new boolean[rows * cols];
+        return backtracking(threshold, rows, cols, 0, 0, visited);
     }
 
-    private static int backtracking(int threshold, int rows, int cols, boolean[] marked, int x, int y){
-        if (x < 0 || x >= rows || y < 0 || y >= cols) return 0;
-        int i = x * cols + y;
-        if (marked[i] || !isValid(threshold, x, y)) return 0;
-        marked[i] = true;
-        return 1 + backtracking(threshold, rows, cols, marked, x+1, y)
-                + backtracking(threshold, rows, cols, marked, x-1, y)
-                + backtracking(threshold, rows, cols, marked, x, y+1)
-                + backtracking(threshold, rows, cols, marked, x, y-1);
+    private int backtracking(int threshold, int rows, int cols, int row, int col, boolean[] visited){
+        if (col < 0 || col >= cols || row < 0 || row >= rows) return 0;
+        int i = cols * row + col;
+        if (visited[i] || !isValid(row, col, threshold)) return 0;
+        visited[i] = true;
+        return backtracking(threshold, rows, cols, row+1, col, visited)
+                + backtracking(threshold, rows, cols, row-1, col, visited)
+                + backtracking(threshold, rows, cols, row, col+1, visited)
+                + backtracking(threshold, rows, cols, row, col-1, visited) + 1;
     }
 
-    private static boolean isValid(int threshold, int x, int y){
-        int sum = 0;
+    private boolean isValid(int x, int y, int threshold){
+        int res = 0;
         while (x != 0){
-            sum += x % 10;
+            res += x % 10;
             x /= 10;
         }
         while (y != 0){
-            sum += y % 10;
+            res += y % 10;
             y /= 10;
         }
-        if (sum > threshold) return false;
+        if (res > threshold) return false;
         return true;
     }
 }
