8a0083d5fe68fe43e81bb59226b65cad967e6051
chenzipeng10 <chenzipeng10@126.com>
2020-02-13 12:40:09 +0800

    TargetOffer_16

---
+++ src/TargetOffer/TargetOffer_16.java
@@ -0,0 +1,25 @@
+package TargetOffer;
+
+import bean.ListNode;
+
+public class TargetOffer_16 {
+    public ListNode Merge(ListNode h1, ListNode h2) {
+        if (h1 == null && h2 == null) return null;
+        if (h1 == null) return h2;
+        if (h2 == null) return h1;
+        ListNode dummy = new ListNode(-1), p = dummy;
+        while (h1 != null && h2 != null){
+            if (h1.val < h2.val){
+                p.next = h1;
+                h1 = h1.next;
+            }else{
+                p.next = h2;
+                h2 = h2.next;
+            }
+            p = p.next;
+        }
+        if (h1 == null) p.next = h2;
+        else p.next = h1;
+        return dummy.next;
+    }
+}
