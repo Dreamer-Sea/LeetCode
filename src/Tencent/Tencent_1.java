bdf79b4d24af56dfe2a1295563b58a430badd226
chenzipeng10 <chenzipeng10@126.com>
2020-02-20 12:52:38 +0800

    Tencent_1

---
+++ src/Tencent/Tencent_1.java
@@ -0,0 +1,29 @@
+package Tencent;
+
+import bean.ListNode;
+
+public class Tencent_1 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        int carry = 0, sum;
+        ListNode dummy = new ListNode(-1), p = dummy;
+        while (l1 != null || l2 != null || carry != 0){
+            sum = 0;
+            if (l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+        }
+        return dummy.next;
+    }
+}
