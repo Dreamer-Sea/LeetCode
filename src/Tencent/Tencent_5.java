494263cdb9896c5e11182ab21821cf2321dd7dd8
chenzipeng10 <chenzipeng10@126.com>
2020-02-21 08:29:03 +0800

    Tencent_5

---
+++ src/Tencent/Tencent_5.java
@@ -0,0 +1,32 @@
+package Tencent;
+
+public class Tencent_5 {
+
+    public static void main(String[] args) {
+        Tencent_5 p = new Tencent_5();
+        p.myAtoi("   -42");
+    }
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) return 0;
+        char[] cs = str.trim().toCharArray();
+        if (cs == null || cs.length == 0) return 0;
+        int i = 0;
+        if (cs[0] == '-' || cs[0] == '+') i++;
+        int res = 0;
+        for (; i < cs.length; i++){
+            if (!Character.isDigit(cs[i])) break;
+            if (res > Integer.MAX_VALUE / 10){
+                if (cs[0] == '-') return Integer.MIN_VALUE;
+                return Integer.MAX_VALUE;
+            }
+            res *= 10;
+            res += cs[i] - '0';
+        }
+        if (cs[0] == '-'){
+            if (-res > 0) return Integer.MIN_VALUE;
+            return -res;
+        }
+        if (res < 0) return Integer.MAX_VALUE;
+        return res;
+    }
+}
