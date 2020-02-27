27e15d664a08337e4251f411dbb6ebe368e8b4b6
chenzipeng10 <chenzipeng10@126.com>
2020-02-27 11:56:16 +0800

    Tencent_23

---
+++ src/Tencent/Tencent_23.java
@@ -0,0 +1,16 @@
+package Tencent;
+
+public class Tencent_23 {
+    public int climbStairs(int n) {
+        if (n < 1) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int a = 1, b = 2, tmp;
+        for (int i = 2; i < n; i++){
+            tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
