7eef17dc33127a0a814877a9e8735db511665cb9
chenzipeng10 <chenzipeng10@126.com>
2020-02-29 08:06:06 +0800

    Tencent_28

---
+++ src/Tencent/Tencent_28.java
@@ -0,0 +1,12 @@
+package Tencent;
+
+public class Tencent_28 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int max = 0;
+        for (int i = 1;i < prices.length; i++){
+            if (prices[i] > prices[i - 1]) max += prices[i] - prices[i - 1];
+        }
+        return max;
+    }
+}
