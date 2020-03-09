4d0138ab718abdc3588d26633da6482c13b3bddc
chenzipeng10 <chenzipeng10@126.com>
2020-03-09 08:33:00 +0800

    Tencent_6

---
+++ src/Tencent/Tencent_6.java
@@ -0,0 +1,16 @@
+package Tencent;
+
+public class Tencent_6 {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int i = 0, j = s.length()-1;
+        while (i < j){
+            if (s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
