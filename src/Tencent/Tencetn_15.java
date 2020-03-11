308e50ad54747486988de1c73ea7da366878709b
chenzipeng10 <chenzipeng10@126.com>
2020-03-12 07:53:41 +0800

    Tencent_15

---
+++ src/Tencent/Tencetn_15.java
@@ -0,0 +1,26 @@
+package Tencent;
+
+public class Tencetn_15 {
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        search(nums, 0);
+    }
+
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length-1;
+        while (left <= right){
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > nums[left]){
+                if (target < nums[mid] && target >= nums[left]) right = mid - 1;
+                else left = mid + 1;
+            }else if (nums[mid] < nums[left]){
+                if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }else left++;
+        }
+        return -1;
+    }
+}
