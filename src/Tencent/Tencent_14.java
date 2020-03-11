8b84c9f8a46fed9e691ccdf8a868c35442c488e6
chenzipeng10 <chenzipeng10@126.com>
2020-03-11 08:30:39 +0800

    Tencent_14

---
+++ src/Tencent/Tencent_14.java
@@ -0,0 +1,15 @@
+package Tencent;
+
+public class Tencent_14 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++){
+            if (nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
