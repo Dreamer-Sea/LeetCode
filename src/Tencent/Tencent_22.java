5b3c6d66fd755a737ce0a7f3781f3cd08e5094c3
chenzipeng10 <chenzipeng10@126.com>
2020-03-14 09:07:49 +0800

    Tencent_22

---
+++ src/Tencent/Tencent_22.java
@@ -0,0 +1,14 @@
+package Tencent;
+
+public class Tencent_22 {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++){
+            for (int j = 1; j < n; j++){
+                dp[j] += dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+}
