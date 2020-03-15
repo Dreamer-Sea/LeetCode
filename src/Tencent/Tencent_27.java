91a7df21488523d2939805fa8e32609a124b9392
chenzipeng10 <chenzipeng10@126.com>
2020-03-16 07:34:04 +0800

    Tencent_27

---
+++ src/Tencent/Tencent_27.java
@@ -0,0 +1,12 @@
+package Tencent;
+
+import bean.TreeNode;
+
+public class Tencent_27 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
