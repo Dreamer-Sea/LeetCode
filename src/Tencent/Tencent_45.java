0558c683d631d9e673a348829f2b4e3ea1b05851
chenzipeng10 <chenzipeng10@126.com>
2020-03-21 08:24:35 +0800

    Tencent_45

---
+++ src/Tencent/Tencent_45.java
@@ -0,0 +1,14 @@
+package Tencent;
+
+import bean.TreeNode;
+
+public class Tencent_45 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+}
