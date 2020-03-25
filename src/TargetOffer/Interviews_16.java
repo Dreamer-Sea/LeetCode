176b76d6600117e256b027ff682b24a15fa48b79
chenzipeng10 <chenzipeng10@126.com>
2020-03-25 11:18:52 +0800

    Interviews_16

---
+++ src/TargetOffer/Interviews_16.java
@@ -0,0 +1,24 @@
+package TargetOffer;
+
+public class Interviews_16 {
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2.0, 10));
+    }
+
+    public static double myPow(double x, int n) {
+       if (x == 0) return 0;
+       long b = n;
+       double res = 1.0;
+       if (b < 0){
+           x = 1 / x;
+           b = -b;
+       }
+       while (b > 0){
+           if ((b & 1) == 1) res *= x;
+           x *= x;
+           b >>= 1;
+       }
+       return res;
+    }
+}
