60e77d5a10334884aa9079f9fffac9d1b0984cf1
chenzipeng10 <chenzipeng10@126.com>
2020-03-27 08:40:00 +0800

    Interviews_26

---
+++ src/TargetOffer/Interviews_26.java
@@ -0,0 +1,14 @@
+package TargetOffer;
+
+import bean.TreeNode;
+
+public class Interviews_26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+    boolean recur(TreeNode A, TreeNode B) {
+        if(B == null) return true;
+        if(A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}
