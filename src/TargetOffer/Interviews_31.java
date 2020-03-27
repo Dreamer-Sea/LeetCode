7f61965990d7c6d067a91cf22bad3a5d035691cc
chenzipeng10 <chenzipeng10@126.com>
2020-03-27 19:11:38 +0800

    Interviews_31

---
+++ src/TargetOffer/Interviews_31.java
@@ -0,0 +1,18 @@
+package TargetOffer;
+
+import java.util.Stack;
+
+public class Interviews_31 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> s = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushed.length; i++){
+            s.push(pushed[i]);
+            while (!s.isEmpty() && s.peek() == popped[j]){
+                s.pop();
+                j++;
+            }
+        }
+        return s.isEmpty();
+    }
+}
