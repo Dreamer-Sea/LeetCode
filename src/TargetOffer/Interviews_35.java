9930ef9d99735ee0bfbd7eb53c5d9823091848a5
chenzipeng10 <chenzipeng10@126.com>
2020-03-28 11:14:47 +0800

    Interviews_34

---
+++ src/TargetOffer/Interviews_35.java
@@ -0,0 +1,26 @@
+package TargetOffer;
+
+import bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Interviews_35 {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        sum(root, res, new ArrayList<>(), sum);
+        return res;
+    }
+
+    private void sum(TreeNode root, List<List<Integer>> res, List<Integer> path, int sum){
+        if (root == null || root.val > sum) return;
+        sum -= root.val;
+        path.add(root.val);
+        if (root.left == null && root.right == null && sum == 0)
+            res.add(new ArrayList<>(path));
+        sum(root.left, res, path, sum);
+        sum(root.right, res, path, sum);
+        path.remove(path.size()-1);
+    }
+}
