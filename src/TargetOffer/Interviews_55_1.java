87fea06af08467da9dadac764cd5781bb3854321
chenzipeng10 <chenzipeng10@126.com>
2020-03-30 12:55:59 +0800

    Interviews_55_1

---
+++ src/TargetOffer/Interviews_55_1.java
@@ -0,0 +1,12 @@
+package TargetOffer;
+
+import bean.TreeNode;
+
+public class Interviews_55_1 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
