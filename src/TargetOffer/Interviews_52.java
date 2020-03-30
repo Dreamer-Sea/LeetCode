abe40eb8cb4c98820c318994b00f3195cc7a8cdf
chenzipeng10 <chenzipeng10@126.com>
2020-03-30 11:03:10 +0800

    Interviews_52

---
+++ src/TargetOffer/Interviews_52.java
@@ -0,0 +1,15 @@
+package TargetOffer;
+
+import bean.ListNode;
+
+public class Interviews_52 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        while (a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
