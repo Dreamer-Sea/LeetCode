09c680443650bc71bb66ee12db2881c89a88ffd8
chenzipeng10 <chenzipeng10@126.com>
2020-03-31 08:22:55 +0800

    Interviews_56_1

--- src/TargetOffer/Interviews_56_1.java
+++ src/TargetOffer/Interviews_56_1.java
@@ -1,24 +1,17 @@
 package TargetOffer;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Interviews_56_1 {
-
-    public static void main(String[] args) {
-        singleNumbers(new int[]{4, 1, 4, 6});
-    }
-
-    public static int[] singleNumbers(int[] nums) {
-       int xor = 0;
+    public int[] singleNumbers(int[] nums) {
+        if (nums == null || nums.length == 0) return new int[0];
+        int xor = 0;
         for (int num : nums) {
             xor ^= num;
         }
         int point = xor & (-xor);
         int[] res = new int[2];
-        for (int num : nums) {
-            if ((point & num) == point) res[0] ^= num;
-            else res[1] ^= num;
+        for (int i = 0; i < nums.length; i++){
+            if ((nums[i] & point) == point) res[0] ^= nums[i];
+            else res[1] ^= nums[i];
         }
         return res;
     }
