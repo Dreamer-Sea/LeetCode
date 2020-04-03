2a22090c430157c6e3d0a1451f4796afcf116315
chenzipeng10 <chenzipeng10@126.com>
2020-04-03 16:59:35 +0800

    Interviews_68_1

---
+++ src/TargetOffer/Interviews_68_1.java
@@ -0,0 +1,11 @@
+package TargetOffer;
+
+import bean.TreeNode;
+
+public class Interviews_68_1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        return root;
+    }
+}
